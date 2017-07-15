6adde8546896efad53deb465ed5446489bb6c2f1
blogscot <iain.diamond@gmail.com>
2017-07-15 15:57:13 +0100

    Construct initial grid state

--- src/js/components/Grid.js
+++ src/js/components/Grid.js
@@ -4,6 +4,22 @@ import '../../css/Grid.css'
 import Col from "./Col";
 
 export default class Grid extends React.Component {
+  constructor() {
+    super()
+    class square {
+      constructor(row, col) {
+      this.position = {row, col}
+      this.active = false
+      this.value = "blank"
+      }
+    }
+    let grid = [
+      [new square(0,0), new square(0,1), new square(0,2)],
+      [new square(1,0), new square(1,1), new square(1,2)],
+      [new square(2,0), new square(2,1), new square(2,2)],
+    ]
+    this.state = { grid }
+  }
   render() {
     return (
       <div className={"centre"}>
