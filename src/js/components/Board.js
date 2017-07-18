ce4f147580808050a1da5f00749083c6736e15bf
blogscot <iain.diamond@gmail.com>
2017-07-18 13:16:56 +0100

    Modify row styles

--- src/js/components/Board.js
+++ src/js/components/Board.js
@@ -55,17 +55,17 @@ export default class Board extends React.Component {
   render() {
     return (
       <div className={"board"}>
-        <div>
+        <div className="row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div>
+        <div className="row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div>
+        <div className="row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
