d384b9006b24a3e69debd363d0e1643ce8452df7
blogscot <iain.diamond@gmail.com>
2017-07-19 20:58:58 +0100

    Update styles

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -6,6 +6,7 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
+        <h1 style={{"textAlign": "center"}}>Tic Tac Toe</h1>
         <Board />
       </div>
     )
--- src/js/components/Status.js
+++ src/js/components/Status.js
@@ -5,10 +5,10 @@ export default function Status(props) {
   const status = props.status
   return (
     <div>
-      <div class="status">{status}&nbsp;&nbsp;
-        <button 
-          onClick={() => props.onClick()}>Restart</button>
-      </div>     
+      <div class="status">{status}</div>     
+      <div class="status">
+        <button onClick={() => props.onClick()}>Restart</button>
+      </div>
     </div>
   )
 }
