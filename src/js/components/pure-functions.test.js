3c8be690367313cc53a69f1bb1931e17fe61894b
blogscot <iain.diamond@gmail.com>
2017-07-26 13:48:32 +0100

    Fix linting warnings

--- src/js/components/pure-functions.test.js
+++ src/js/components/pure-functions.test.js
@@ -6,61 +6,66 @@ import {
 
 describe('Find a winner', () => {
   test('find a winner on first row', () => {
-    let squares = ['X','X','X',
-                   null,null,null,
-                   null,null,null]
+    let squares = [
+      'X','X','X',
+      null,null,null,
+      null,null,null]
 
     expect(findWinner(squares)).toBe('X')
   })
 
   test('find a winner on last row', () => {
-    let squares = [null,'X',null,
-                   'X',null,null,
-                   'O','O','O']
+    let squares = [
+      null,'X',null,
+      'X',null,null,
+      'O','O','O']
 
     expect(findWinner(squares)).toBe('O')
   })
 
   test('find a winner on first column', () => {
-    let squares = ['O','X',null,
-                   'O',null,null,
-                   'O','X','X']
+    let squares = [
+      'O','X',null,
+      'O',null,null,
+      'O','X','X']
 
     expect(findWinner(squares)).toBe('O')
   })
 
   test('find a winner on middle column', () => {
-    let squares = ['O','X',null,
-                   null,'X',null,
-                   'O','X',null]
+    let squares = [
+      'O','X',null,
+      null,'X',null,
+      'O','X',null]
 
     expect(findWinner(squares)).toBe('X')
   })
 
   test('find a winner on diagonal', () => {
-    let squares = ['O','X',null,
-                   null,'O',null,
-                   null,'X','O']
+    let squares = [
+      'O','X',null,
+      null,'O',null,
+      null,'X','O']
 
     expect(findWinner(squares)).toBe('O')
   })
-
 })
 
 describe('Moves left?', () => {
   test('A non-empty board has moves left', () => {
-    let squares = ['X','X','X',
-                   null,null,null,
-                   null,null,null]
+    let squares = [
+      'X','X','X',
+      null,null,null,
+      null,null,null]
 
     expect(movesLeft(squares)).toBeTruthy()
   })
 
   test('An empty board has no moves left', () => {
-    let squares = ['X','X','X',
-                   'X','X','X',
-                   'X','X','X'
-                  ]
+    let squares = [
+      'X','X','X',
+      'X','X','X',
+      'X','X','X']
 
     expect(movesLeft(squares)).toBeFalsy()
   })    
@@ -68,27 +73,28 @@ describe('Moves left?', () => {
 
 describe('Update Game Status', () => {
   test('A game has a winner', () => {
-    let squares = ['X','X','X',
-                   null,null,null,
-                   null,null,null]
+    let squares = [
+      'X','X','X',
+      null,null,null,
+      null,null,null]
 
-    expect(updateGameStatus(squares, 'O')).toBe("Winner: X")
+    expect(updateGameStatus(squares, 'O')).toBe('Winner: X')
   })
 
   test('A game is in progress', () => {
-    let squares = ['null','O','X',
-                   'O',null,'X',
-                   'X','O',null
-                  ]
+    let squares = [
+      'null','O','X',
+      'O',null,'X',
+      'X','O',null]
 
     expect(updateGameStatus(squares, 'X')).toBe('Next player: O')
   })    
 
   test('A game to be a draw', () => {
-    let squares = ['O','X','O',
-                   'O','X','O',
-                   'X','O','X'
-                  ]
+    let squares = [
+      'O','X','O',
+      'O','X','O',
+      'X','O','X']
 
     expect(updateGameStatus(squares, 'O')).toBe('Game is drawn')
   })    
@@ -103,5 +109,4 @@ describe('Players use Xs and Os', () => {
   test('The next player is O', () => {
     expect(nextPlayer('X')).toBe('O')
   })
-
 })
