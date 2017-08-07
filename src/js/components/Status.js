59b53b9c9dda1e885b73645ebb1d046d8d03ba1e
iain <iain.diamond@gmail.com>
2017-08-07 20:48:16 +0100

    Fix Restart button

--- src/js/components/Status.js
+++ src/js/components/Status.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from 'react-bootstrap'
 
-export default function Status({status}) {
+export default function Status({status, onClick}) {
   return (
     <div>
       <div style={nextPlayerStyle}>{status}</div>     
       <div style={nextPlayerStyle}>
         <Button 
           bsStyle="primary" 
-          onClick={() => props.onClick()}> Restart </Button>
+          onClick={onClick}> Restart </Button>
       </div>
     </div>
   )
